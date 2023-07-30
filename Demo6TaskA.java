9ebce5de66abab0cd5347b1b5a2429cd67861055
Sahan Lakmal <sahanloser@gmail.com>
2023-07-30 18:12:39 +0530

    Complete Task 6-A

--- Demo6TaskA.java
+++ Demo6TaskA.java
@@ -1,16 +1,13 @@
 public class Demo6TaskA {
     public static void main(String[] args) {
 
-        // final String RED = "\033[31m";
-        
-        // System.out.printf("[%8s%d%d%-8s]\n","",5,0,"%");
+        //Create color variables
 
         final String RED_BG = "\033[41m";
         final String GREEN_BG = "\033[42m";
         final String WHITE = "\033[37m";
         final String RESET = "\033[0m";
 
-        
 
         System.out.printf("%s[%s%-8s%s%d%s%-8s%s]%s\n", RED_BG, WHITE, " ", WHITE, 5, GREEN_BG, "0%", GREEN_BG, RESET);//Method 1
 
