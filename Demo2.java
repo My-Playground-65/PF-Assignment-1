e7d67e8b3cb3eb4787b7d192493f706824bced42
Sahan Lakmal <sahanloser@gmail.com>
2023-07-30 10:46:16 +0530

    Complete Task 2

---
+++ Demo2.java
@@ -0,0 +1,9 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        //Create constant PI varaible
+        final double PI = Math.PI;
+        
+        //Print PI value with only 3 decimal places using printf
+        System.out.printf("%.3f\n",PI);
+    }
+}
