76fd7e3834ceb170cf480c598874980b0af838b0
Sahan Lakmal <sahanloser@gmail.com>
2023-07-30 10:32:19 +0530

    Complete Task 1

---
+++ Demo1.java
@@ -0,0 +1,11 @@
+public class Demo1 {
+    public static void main(String[] args) {
+       //Create a double varable 
+       double bankSaving = 123456.7890;
+       
+       //Use printf to display above amount with thousand grouping and with only 2 decimal places
+
+       System.out.printf("%,.2f\n",bankSaving);
+       
+    }
+}
